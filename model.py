4595ec21f7827ce6cedbc27013b89b34366ebf96
Khubbatulin <markhubbatulin@gmail.com>
2021-04-23 03:14:06 +0300

    resnet101 v1.4

--- model.py
+++ model.py
@@ -6,7 +6,7 @@ from utils import NUM_PTS, CROP_SIZE
 
 def create_model():
     model = models.resnet101(pretrained=True)
-    model.requires_grad_(False)
+    model.requires_grad_(True)
 
     model.fc = nn.Linear(model.fc.in_features, 2 * NUM_PTS, bias=True)
     model.fc.requires_grad_(True)
